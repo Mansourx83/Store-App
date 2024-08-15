be4d05151f3936ff0f927535a9c4a2bd69cf57e4
Mansourx83 <yousaf83000@gmail.com>
2024-08-16 01:16:42 +0300

    add features

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -4,16 +4,17 @@ class ProductModel {
   final double price;
   final String description;
   final String image;
+  final String category;
   final RatingModel rating;
 
-  ProductModel({
-    required this.id,
-    required this.title,
-    required this.price,
-    required this.description,
-    required this.image,
-    required this.rating,
-  });
+  ProductModel(
+      {required this.id,
+      required this.title,
+      required this.price,
+      required this.description,
+      required this.image,
+      required this.rating,
+      required this.category});
 
   factory ProductModel.fromJson(Map<String, dynamic> jsonData) {
     return ProductModel(
@@ -23,6 +24,7 @@ class ProductModel {
           (jsonData['price'] as num).toDouble(), // Ensure 'price' is a double
       description: jsonData['description'] as String,
       image: jsonData['image'] as String,
+      category: jsonData['category'] as String,
       rating: RatingModel.fromJson(jsonData['rating'] as Map<String, dynamic>),
     );
   }
--- lib/screens/updata_product_page.dart
+++ lib/screens/updata_product_page.dart
@@ -1,52 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:store_app/models/product_model.dart';
+import 'package:store_app/services/update_product.dart';
+import 'package:store_app/widgets/mybar.dart';
 import 'package:store_app/widgets/mybutton.dart';
 import 'package:store_app/widgets/mytextfield.dart';
 
-class UpdateProductPage extends StatelessWidget {
+class UpdateProductPage extends StatefulWidget {
   static String id = 'Update Page';
-  String? productName, description, image;
-  int? price;
+
+  @override
+  State<UpdateProductPage> createState() => _UpdateProductPageState();
+}
+
+class _UpdateProductPageState extends State<UpdateProductPage> {
+  String? productName, description, image, price;
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Update Product'),
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            MyTextField(
-              hint: 'Product Name',
-              onCahnged: (data) {
-                productName = data;
-              },
-            ),
-            MyTextField(
-              hint: 'Description',
-              onCahnged: (data) {
-                description = data;
-              },
-            ),
-            MyTextField(
-              hint: 'Price',
-              onCahnged: (data) {
-                price = int.parse(data);
-              },
-              typeOfText: TextInputType.number,
-            ),
-            MyTextField(
-              hint: 'Image',
-              onCahnged: (data) {
-                image = data;
-              },
-            ),
-            MyButton(text: 'Updata', function: () {}),
-          ],
+    ProductModel product =
+        ModalRoute.of(context)!.settings.arguments as ProductModel;
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('Update Product'),
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              MyTextField(
+                hint: 'Product Name',
+                onCahnged: (data) {
+                  productName = data;
+                },
+              ),
+              MyTextField(
+                hint: 'Description',
+                onCahnged: (data) {
+                  description = data;
+                },
+              ),
+              MyTextField(
+                hint: 'Price',
+                onCahnged: (data) {
+                  // price = int.parse(data);
+                  price = data;
+                },
+                typeOfText: TextInputType.number,
+              ),
+              MyTextField(
+                hint: 'Image',
+                onCahnged: (data) {
+                  image = data;
+                },
+              ),
+              MyButton(
+                  text: 'Updata',
+                  function: () {
+                    isLoading = true;
+                    setState(() {});
+                    try {
+                      updateproduct(product);
+                      showBar(context,
+                          text: 'Success Update', color: Colors.green);
+                    } catch (e) {
+                      showBar(context,
+                          text: 'Failed Update', color: Colors.red);
+                      print('Error is : ${e.toString()}');
+                    }
+                    isLoading = false;
+                    setState(() {});
+                  }),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  void updateproduct(ProductModel product) {
+    UpdateProduct().updateProduct(
+      title: productName!,
+      price: price!,
+      description: description!,
+      image: image!,
+      category: product.category,
     );
   }
 }
